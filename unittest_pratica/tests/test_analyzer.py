ec117a0cdaef3ae513a03fe4a3dbef94b2e95065
AugustoMagalhaes <augustommg@gmail.com>
2023-11-26 23:52:58 -0300

    arquivos iniciais para o unittest mock

---
+++ unittest_pratica/tests/test_analyzer.py
@@ -0,0 +1,6 @@
+from analyzer import analyze_json_file
+
+
+def test_analyze_json_file():
+    result = analyze_json_file("person_data.json")
+    assert result == "A pessoa de nome João tem 30 anos de idade."
