6bcd9b7c1d737bb4add51b076c54e4994bd801b6
AugustoMagalhaes <augustommg@gmail.com>
2023-11-27 00:57:02 -0300

    cria teste para quando arquivo eh invalido

--- unittest_pratica/analyzer.py
+++ unittest_pratica/analyzer.py
@@ -2,16 +2,16 @@ import json
 
 
 def read_json_file(file_path):
-    with open(file_path, "r", encoding="utf-8") as file:
+    with open(file_path, "r") as file:
         return json.load(file)
 
 
-def analyze_json_file(file_path) -> str:
+def analyze_json_file(file_path):
     if not file_path.endswith(".json"):
         raise ValueError("O arquivo precisa ser um arquivo JSON.")
 
     data = read_json_file(file_path)
     return (
         f"A pessoa de nome {data['nome']} "
-        f"tem {data['idade']} anos de idade."
+        f"possui {data['idade']} anos de idade."
     )
