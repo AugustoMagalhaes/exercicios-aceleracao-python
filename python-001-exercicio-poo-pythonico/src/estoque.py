d5602d809f786feeec2fa5a0bc2f07365e6f4999
AugustoMagalhaes <augustommg@gmail.com>
2023-12-27 04:45:34 -0300

    r02: implementa classe Estoque

--- python-001-exercicio-poo-pythonico/src/estoque.py
+++ python-001-exercicio-poo-pythonico/src/estoque.py
@@ -3,18 +3,32 @@ from typing import Dict
 
 class Estoque:
     def __init__(self, produtos: Dict[str, int]) -> None:
-        pass
+        self.produtos = produtos
 
     def adicionar_produto_no_estoque(self, nome: str, quantidade: int) -> None:
-        pass
+        if nome in self.produtos:
+            qtd = self.produtos.get(nome, 0)
+            self.produtos.update({nome: qtd + quantidade})
+        else:
+            self.produtos.setdefault(nome, quantidade)
 
     def remover_produto_do_estoque(self, nome: str, quantidade: int) -> None:
-        pass
+        if quantidade > self.produtos.get(nome, 0):
+            raise ValueError
+        if nome in self.produtos:
+            qtd = self.produtos.get(nome, 0)
+            self.produtos.update({nome: qtd - quantidade})
+        else:
+            self.produtos.pop(nome, None)
 
     def atualizar_produto_no_estoque(
         self, nome: str, nova_quantidade: int
     ) -> None:
-        pass
+        if nome not in self.produtos:
+            raise ValueError
+        self.produtos.update({nome: nova_quantidade})
 
     def visualizar_estoque(self) -> None:
-        pass
+        lista_produtos_msg = [f"{k}: {v}" for k, v in self.produtos.items()]
+        msg = "\n".join(lista_produtos_msg)
+        print(msg)
