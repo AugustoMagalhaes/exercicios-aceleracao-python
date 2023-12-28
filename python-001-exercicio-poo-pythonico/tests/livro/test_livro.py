38f1ea28e5ceca028234fd759168a3b189145ca5
AugustoMagalhaes <augustommg@gmail.com>
2023-12-28 00:38:36 -0300

    r03: cria teste pra instanciacao de Livro

--- python-001-exercicio-poo-pythonico/tests/livro/test_livro.py
+++ python-001-exercicio-poo-pythonico/tests/livro/test_livro.py
@@ -1,5 +1,9 @@
-# from src.livro.livro import Livro
+from src.livro.livro import Livro
 
 
 def test_cria_livro():
-    pass  # Seu teste deve ser escrito aqui
+    livro = Livro("titulo", "autor", 100)
+    titulo, autor, paginas = livro.titulo, livro.autor, livro.paginas
+    assert titulo == "titulo"
+    assert autor == "autor"
+    assert paginas == 100
