4c31e973106f68b5392b2d902720b28758d45572
AugustoMagalhaes <augustommg@gmail.com>
2023-12-28 00:53:04 -0300

    r04: cria testes para descricao do livro

--- python-001-exercicio-poo-pythonico/tests/descricao_livro/test_descricao_livro.py
+++ python-001-exercicio-poo-pythonico/tests/descricao_livro/test_descricao_livro.py
@@ -1,5 +1,14 @@
-# from src.livro.livro import Livro
+from src.livro.livro import Livro
 
 
-def test_descricao_livro():
-    pass  # Colocar sua implementação aqui
+def test_descricao_livro(capsys):
+    livro = Livro("Um Titulo", "Um Autor", 100)
+    print(livro)
+    captured = capsys.readouterr()
+    output = captured.out
+    expected = (
+        "O livro Um Titulo"
+        " de Um Autor"
+        " possui 100 páginas.\n"
+    )
+    assert output == expected
