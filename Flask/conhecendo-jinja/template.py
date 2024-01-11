f5ce7b9c710d39778667faff9167d9b4e47add77
AugustoMagalhaes <augustommg@gmail.com>
2024-01-11 02:48:24 -0300

    modifica templates para dinamizar html

--- Flask/conhecendo-jinja/template.py
+++ Flask/conhecendo-jinja/template.py
@@ -4,9 +4,13 @@ from jinja2 import Template
 template_file = open('template.html').read()
 template = Template(template_file)
 
+data = {
+    'saudacao': 'Eu sou um template HTML',
+    'informacao': ('E essa é uma das formas de se passar '
+                   'múltiplas informações para o template'),
+    'contexto': 'Isso é possível através da criação de um contexto'
+}
 
-SAUDACAO = 'Eu sou um template HTML'
-
-output = template.render(saudacao=SAUDACAO)
+output = template.render(data)
 
 print(output)
