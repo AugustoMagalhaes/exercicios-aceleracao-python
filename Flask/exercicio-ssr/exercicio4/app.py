550dcb91ffca5510f48950ee232e6de5ca341e1f
AugustoMagalhaes <augustommg@gmail.com>
2024-01-19 00:02:51 -0300

    finaliza exercicio 5 e 6

--- Flask/exercicio-ssr/exercicio4/app.py
+++ Flask/exercicio-ssr/exercicio4/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, abort, url_for
 app = Flask(__name__)
 
 products = []
@@ -11,8 +11,21 @@ def index():
         global products
         products = [*products, product] if product else products
         product = None
-        return render_template('index.html', products=products)
+        return redirect(url_for("index"))
     return render_template('index.html', products=products)
+
+
+@app.route("/delete_product/<product>", methods=["POST"])
+def delete_product(product):
+    # Encontra o produto na lista pelo id
+    product = next((p for p in products if p == product), None)
+    # Se o produto não existir, retorna um erro 404
+    if product is None:
+        abort(404, f"Produto de nome '{product}' não encontrado.")
+    # Se o produto existir, remove ele da lista
+    products.remove(product)
+    # Retorna uma mensagem de sucesso 200
+    return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
